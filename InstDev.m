03860bba8c7c2750e6c02c5cbdccae54449338e2
Pieski <tinesharp@outlook.com>
2024-03-04 23:47:20 +0800

    Fulfilled GUI functions

--- InstDev.m
+++ InstDev.m
@@ -1,18 +1,31 @@
-classdef InstDev
+% A value class constructor returns an object that is associated with the variable to which it is assigned. If you reassign this variable, MATLAB® creates an independent copy of the original object. If you pass this variable to a function to modify it, the function must return the modified object as an output argument. For information on value-class behavior, see Avoid Unnecessary Copies of Data.
+% 
+% A handle class constructor returns a handle object that is a reference to the object created. You can assign the handle object to multiple variables or pass it to functions without causing MATLAB to make a copy of the original object. A function that modifies a handle object passed as an input argument does not need to return the object.
+% 
+% All handle classes are derived from the abstract handle class.
+%
+% https://www.mathworks.com/help/matlab/matlab_oop/comparing-handle-and-value-classes.html
+classdef InstDev < handle
 
     properties (GetAccess = public, SetAccess = protected)
         IDN
         isConnected = false
         acquireDepth
-        mainTimeBase
+        mainTimebase
+        focus
         xIncrement
         xOrigin
         xRef
+        yRange
         yIncrement
         yOrigin
         yRef
         yOffset
         averageCount
+        triggerMode
+        triggerSource
+        triggerLevel
+        triggerEdge
     end
 
     properties (GetAccess = protected, SetAccess = protected)
@@ -21,7 +34,6 @@ classdef InstDev
 
     properties (GetAccess = public, SetAccess = immutable, Abstract)
         channelN
-        channelRange
         acquireDepthLevel
         maxBatch
     end
@@ -36,6 +48,8 @@ classdef InstDev
                 obj.yOrigin = zeros(1, obj.channelN);
                 obj.yRef = zeros(1, obj.channelN);
                 obj.yOffset = zeros(1, obj.channelN);
+                obj.yRange = zeros(1, obj.channelN);
+                obj.focus = zeros(1,obj.channelN);
             catch
                 error('Failed to connect VISA device.');
             end
@@ -52,6 +66,10 @@ classdef InstDev
         setMainTimebase(obj, scale)
         setChannelStatus(obj, chan, onoff)
         setChannelRange(obj, chan, range)
+
+        % mode = 1=AUTO/2=NORM/3=SING, chan = 1/2/3..., edge = 1=POS/2=NEG/3=ANY
+        setTrigger(obj, mode, chan, edge, level)
+
         getWfm(obj, chan)
         getWfmPreview(obj, chan)
     end
--- Inst_DS1000ZE.m
+++ Inst_DS1000ZE.m
@@ -1,4 +1,5 @@
 classdef Inst_DS1000ZE < InstDev
+
     properties (GetAccess = public, SetAccess = immutable)
         channelN = 2
         channelRange
@@ -13,21 +14,31 @@ classdef Inst_DS1000ZE < InstDev
         end
 
         function sucs = refresh(obj)
-            obj.mainTimeBase = str2double(writeread(obj.instDev, ':TIM:MAIN:SCAL?'));
+            obj.mainTimebase = str2double(writeread(obj.instDev, ':TIM:MAIN:SCAL?'));
 
-            par;
+            par = [];
             for chan = [1 2]
-                par = getWfmParameter(obj.instDev, chan);
-                obj.yIncrement(chan) = par.yincrement;
+                par = obj.getWfmParameter(chan);
+                obj.yIncrement(chan) = par.yIncrement;
                 obj.yOrigin(chan) = par.yOrigin;
                 obj.yRef(chan) = par.yRef;
                 obj.yOffset(chan) = str2double(writeread(obj.instDev,...
-                    append(':CHANnel',num2str(chan),':OFFSet?')));
+                    append(':CHAN',num2str(chan),':OFFS?')));
+                obj.yRange(chan) = str2double(writeread(obj.instDev,...
+                    append(':CHAN',num2str(chan),':RANG?')));
+                obj.focus(chan) = str2double(writeread(obj.instDev,...
+                    append(':CHAN',num2str(chan),':DISP?')));
             end
             obj.xIncrement = par.xIncrement;
             obj.xOrigin = par.xOrigin;
             obj.xRef = par.xRef;
             obj.averageCount = par.averageCount;
+            
+            trig = obj.getTrigger();
+            obj.triggerEdge = trig.edge;
+            obj.triggerLevel = trig.level;
+            obj.triggerSource = trig.chan;
+            obj.triggerMode = trig.mode;
         end
 
         function [readback,sucs] = setAcquireDepth(obj, depth)
@@ -42,6 +53,7 @@ classdef Inst_DS1000ZE < InstDev
             else
                 sucs = false;
             end
+            obj.acquireDepth = readback;
         end
 
         %scale must be a number(scientific notation/irrational is availble) instead of string.
@@ -57,6 +69,7 @@ classdef Inst_DS1000ZE < InstDev
             else
                 sucs = false;
             end
+            obj.mainTimebase = readback;
         end
 
         %TO BE CONFIRMED: It seems that waveform of a not-enabled channel could be
@@ -72,10 +85,11 @@ classdef Inst_DS1000ZE < InstDev
             else
                 sucs = false;
             end
+            obj.focus(chan) = readback;
         end
 
         function [readback,sucs] = setChannelRange(obj, chan, range)
-            if (setChannelStatus(obj.instDev, chan, 1) == false)
+            if (obj.setChannelStatus(chan, 1) == false)
                 error(append('Failed to enable channel', num2str(chan)));
                 return;
             end
@@ -89,24 +103,62 @@ classdef Inst_DS1000ZE < InstDev
             else
                 sucs = false;
             end
+            obj.yRange(chan) = readback;
+        end
+
+        function [readback,sucs] = setTrigger(obj, mode, chan, edge, level)
+            if (mode == 1)
+                write(obj.instDev, ':TRIG:SWE AUTO');
+            elseif (mode == 2)
+                write(obj.instDev, ':TRIG:SWE NORM');
+            else
+                write(obj.instDev, ':TRIG:SWE SING');
+            end
+            
+            write(obj.instDev, append(':TRIG:EDG:SOUR CHAN', num2str(chan)));
+
+            write(obj.instDev, append(':TRIG:EDG:LEV ', num2str(level)));
+
+            if (edge == 1)
+                write(obj.instDev, ':TRIG:EDG:SLOP POS');
+            elseif(edge == 2)
+                write(obj.instDev, ':TRIG:EDG:SLOP NEG');
+            else
+                write(obj.instDev, ':TRIG:EDG:SLOP RFAL');
+            end
+
+            readback = obj.getTrigger();
+
+            if (readback.mode == mode && readback.edge == edge &&...
+                    readback.chan == chan && readback.level == level)
+                sucs = true;
+            else
+                sucs = false;
+            end
+
+            obj.triggerEdge = readback.edge;
+            obj.triggerLevel = readback.level;
+            obj.triggerSource = readback.chan;
+            obj.triggerMode = readback.mode;
+            
         end
 
         function [wfm,t] = getWfm(obj, chan)
-            par = getWfmParameter(obj.instdev, chan);
+            par = obj.getWfmParameter(chan);
             wfm = zeros(1, par.points);
             readcount = 0;
     
             % Read wfm in several batches.
             while (readcount < par.points)
-                write(obj.instdev, append(':WAV:STAR ', num2str(readcount + 1)));
+                write(obj.instDev, append(':WAV:STAR ', num2str(readcount + 1)));
     
                 if (par.points - readcount <= 250000)       % The rest of the data can be read in one batch.
-                    write(obj.instdev, append(':WAV:STOP ', num2str(par.points)));
+                    write(obj.instDev, append(':WAV:STOP ', num2str(par.points)));
                 else                                        % The rest of the data is larger than a max batch.
-                    write(obj.instdev, append(':WAV:STOP ', num2str(readcount + 250000)));
+                    write(obj.instDev, append(':WAV:STOP ', num2str(readcount + 250000)));
                 end
     
-                rawdata = writeread(obj.instdev, ':WAV:DATA?');
+                rawdata = writeread(obj.instDev, ':WAV:DATA?');
     
                 % Read the TMC header in each batch.
                 rawdata = str2mat(rawdata);
@@ -119,10 +171,10 @@ classdef Inst_DS1000ZE < InstDev
             end
             
             % Decode raw wfm with parameters.
-            wfm = (wfm-par.yref)*par.yincrement;
-            wfm = wfm(1:length(wfm-1));          % Seems that the last point is much to low
+            wfm = (wfm-par.yRef)*par.yIncrement - par.yOffset;
+            wfm = wfm(1:length(wfm)-1);          % Seems that the last point is much to low
             t = 1:(par.points-1);
-            t = t*par.xincrement + par.xorigin;
+            t = t*par.xIncrement + par.xOrigin;
         end
 
         function [wfm,t] = getWfmPreview(obj, chan)
@@ -134,12 +186,12 @@ classdef Inst_DS1000ZE < InstDev
 
     methods (Access = private)
 
-        function wfmpara = getWfmParameter(instdev, chan)
+        function wfmpara = getWfmParameter(obj, chan)
             % Set source to channel N.
-            write(instdev, append(':WAV:SOUR CHAN', num2str(chan)));
+            write(obj.instDev, append(':WAV:SOUR CHAN', num2str(chan)));
     
             % Read parameter of channel N.
-            parstr = writeread(instdev, ':WAVeform:PREamble?');
+            parstr = writeread(obj.instDev, ':WAVeform:PREamble?');
             parcell = strsplit(parstr, ',');
             parcell = str2double(parcell);
 
@@ -153,6 +205,8 @@ classdef Inst_DS1000ZE < InstDev
             wfmpara.yIncrement = parcell(8);
             wfmpara.yOrigin = parcell(9);
             wfmpara.yRef = parcell(10);
+            wfmpara.yOffset = str2double(writeread(obj.instDev,...
+                    append(':CHAN',num2str(chan),':OFFS?')));
         end
 
         function [readback,sucs] = setWaveformMode(obj, mode)
@@ -160,11 +214,38 @@ classdef Inst_DS1000ZE < InstDev
             write(obj.instDev, cmd);
 
             readback = writeread(obj.instDev, ':WAVeform:MODE?');
-            if (strcmp(feedback, mode))             % Must use NORM instead of NORMal
+            if (strcmp(readback, mode))             % Must use NORM instead of NORMal
                 sucs = true;
             else
                 sucs = false;
             end
+        end
+
+        function readback = getTrigger(obj)
+            mode = writeread(obj.instDev, ':TRIG:SWE?');
+            if(strncmp(mode, 'AUTO', 4))
+                readback.mode = 1;
+            elseif(strncmp(mode, 'NORM', 4))
+                readback.mode = 2;
+            else
+                readback.mode = 3;
+            end
+            
+            readback.chan = writeread(obj.instDev, ':TRIG:EDG:SOUR?');
+            readback.chan = str2mat(readback.chan);
+            readback.chan = str2double(readback.chan(5));
+
+            edge = writeread(obj.instDev, ':TRIG:EDG:SLOP?');
+            if(strncmp(edge, 'POS', 3))
+                readback.edge = 1;
+            elseif(strncmp(edge, 'NEG', 3))
+                readback.edge = 2;
+            else
+                readback.edge = 3;
+            end
+
+            readback.level = writeread(obj.instDev, 'TRIG:EDG:LEV?');
+            readback.level = str2double(readback.level);
         end
 
     end
