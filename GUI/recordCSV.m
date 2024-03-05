ac706d6d893aa1933af28637f9af9895d9ce9ed6
Pieski <tinesharp@outlook.com>
2024-03-05 23:40:03 +0800

    debug

--- GUI/recordCSV.m
+++ GUI/recordCSV.m
@@ -1,14 +1,18 @@
 function recordCSV(path, dat, chan, chanRange)
-    fileName = string(datetime(), 'LongScope-hhmmss-SSSS');
-    fileName = append(path, fileName);
+    if (~exist(path,'dir'))
+        mkdir(path);
+    end
+
+    fileName = string(datetime(), 'HHmmss-SSSS');
+    fileName = append(path, 'LongScope-', fileName);
 
     % Storage channel info into a string matrix
-    chanInfo = ['Channel' 'Channel Voltage Range (V)'];
+    chanInfo = ["Time", string(datetime(),"yyyy-MM-dd HH:mm:ss.SSSS");"Channel", "Channel Voltage Range (V)"];
     for i = length(chan)
-        chanInfo = [chanInfo; string(chan(i)) chanRange(i)];
+        chanInfo = [chanInfo; string(chan(i)), chanRange(i)];
     end
 
     % Write waveform points. Rotate row vector
     writematrix(dat', append(fileName,'.csv'));
-    writematrix(chanInfo, append(filename,'.info.csv'));
+    writematrix(chanInfo, append(fileName,'.info.csv'));
 end
